5cde8b4e7a41ead80f07526eabaaf9fc93f6f85c
Charley <charley.geoffroy@etu.umontpellier.fr>
2023-06-25 17:49:11 +0200

    feat/ client grpc ask repo name

--- grpc-client-debug/src/main.rs
+++ grpc-client-debug/src/main.rs
@@ -1,15 +1,20 @@
+use std::io;
 use paastech_proto::gitrepomanager::git_repo_manager_client::GitRepoManagerClient;
 use paastech_proto::gitrepomanager::RepositoryRequest;
-
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Create a connection to the gRPC server
     let mut client = GitRepoManagerClient::connect("http://[::1]:50051").await?;
 
+    // Prompt the user to enter the repository name
+    println!("Enter the repository name:");
+    let mut repository_name = String::new();
+    io::stdin().read_line(&mut repository_name)?;
+
     // Prepare the request to create a repository
     let request = tonic::Request::new(RepositoryRequest {
-        repository_path: "charley".to_owned(),
+        repository_path: repository_name.trim().to_owned(),
     });
 
     // Call the create_repository RPC method
@@ -21,3 +26,4 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     Ok(())
 }
+
