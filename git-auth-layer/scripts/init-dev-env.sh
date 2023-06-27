18e40038ac18679ee23a55cc01bc24117405a5e5
Martin Moreira de Jesus <martin.moreira-de-jesus@clicnwork.io>
2023-06-27 19:58:12 +0200

    misc(auth-layer): update testing environment

--- git-auth-layer/scripts/init-dev-env.sh
+++ git-auth-layer/scripts/init-dev-env.sh
@@ -4,10 +4,10 @@ rm -rf scripts/output/client* scripts/server
 
 mkdir -p scripts/output/client-a scripts/output/client-b scripts/output/server
 
-ssh-keygen -t ed25519 -C "gitstake-userA@paastech.fr" -f scripts/output/client-a/id_ed25519 -q -N ""
+ssh-keygen -t ed25519 -C "userA@user.fr" -f scripts/output/client-a/id_ed25519 -q -N ""
 
-ssh-keygen -t ed25519 -C "gitstake-userB@paastech.fr" -f scripts/output/client-b/id_ed25519 -q -N ""
+ssh-keygen -t ed25519 -C "userB@user.fr" -f scripts/output/client-b/id_ed25519 -q -N ""
 
-echo "command=\"/app/gitstake userA\" $(cat scripts/output/client-a/id_ed25519.pub)" > scripts/output/server/authorized_keys
+echo "command=\"/usr/bin/git-auth-layer 1\" $(cat scripts/output/client-a/id_ed25519.pub)" > scripts/output/server/authorized_keys
 
-echo "command=\"/app/gitstake userB\" $(cat scripts/output/client-b/id_ed25519.pub)" >> scripts/output/server/authorized_keys
+echo "command=\"/usr/bin/git-auth-layer 2\" $(cat scripts/output/client-b/id_ed25519.pub)" >> scripts/output/server/authorized_keys
