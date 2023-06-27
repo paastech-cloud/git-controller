731f28953b02811eb38530ed8f8f803342690faf
Martin Moreira de Jesus <martin.moreira-de-jesus@clicnwork.io>
2023-06-27 09:35:16 +0200

    feat(auth-layer): add logging

--- git-auth-layer/src/main.rs
+++ git-auth-layer/src/main.rs
@@ -1,17 +1,38 @@
 mod constants;
 mod utils;
 
+use log::LevelFilter;
+use log4rs::{
+    append::file::FileAppender,
+    config::{Appender, Root},
+    encode::pattern::PatternEncoder,
+    Config,
+};
 use std::process::{exit, Command};
-
 use utils::{check_user_repository_access, get_repo_name};
 
 use crate::constants::{NOT_FOUND_ERROR, UNEXPECTED_ERROR};
 
 #[tokio::main]
 async fn main() {
+    let logfile = FileAppender::builder()
+        .encoder(Box::new(PatternEncoder::new("{l} - {m}\n")))
+        .build("/var/paastech/git-auth-layer/log/output.log")
+        .unwrap();
+
+    let config = Config::builder()
+        .appender(Appender::builder().build("logfile", Box::new(logfile)))
+        .build(Root::builder().appender("logfile").build(LevelFilter::Info))
+        .unwrap();
+
+    let _ = log4rs::init_config(config);
+
+    log::info!("Client connected");
+
     // Extract args there should be the command itself and a string to identify the user
     let args: Vec<String> = std::env::args().collect();
     if args.len() != 2 {
+        log::error!("Args : {:?} malformed", args);
         eprintln!("{}", &UNEXPECTED_ERROR);
         exit(1);
     }
@@ -20,12 +41,12 @@ async fn main() {
     let repository_path = match get_repo_name() {
         Some(value) => value,
         None => {
+            log::error!("Failed extracting repository name from command");
             eprintln!("{}", UNEXPECTED_ERROR);
             exit(1);
         }
     };
 
-    // query database
     let repository_name = repository_path.replace("/srv/", "");
     match check_user_repository_access(user_id, repository_name).await {
         Ok(_) => {
@@ -36,10 +57,16 @@ async fn main() {
                 .ok();
         }
         Err(sqlx::Error::RowNotFound) => {
+            log::info!(
+                "Client {} did not have access to repository {}",
+                user_id,
+                repository_path
+            );
             eprintln!("{}", &NOT_FOUND_ERROR);
             exit(1);
         }
         Err(_) => {
+            log::error!("Unexpected error when querying database");
             eprintln!("{}", &UNEXPECTED_ERROR);
             exit(1);
         }
