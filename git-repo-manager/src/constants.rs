be112c5bc2e453cb696b0c4586d47df3deb5fca7
Martin Moreira de Jesus <martin.moreira-de-jesus@clicnwork.io>
2023-06-28 12:48:53 +0200

    fix(repo-manager): normalize env vars
    Create constant files to store environment variables keys and normalize names.
    Panic with error message when env var is missing.
    Remove personal information from env var and put absolute paths.
    Rename git hook environment variable key.

---
+++ git-repo-manager/src/constants.rs
@@ -0,0 +1,6 @@
+// Env
+// Full path to the repositories location
+pub static GIT_REPOSITORY_BASE_PATH_KEY: &str = "GIT_REPOSITORY_BASE_PATH";
+pub static SERVER_PORT_KEY: &str = "SERVER_PORT";
+// Full path tho the directory containing the hooks
+pub static GIT_HOOKS_BASE_PATH_KEY: &str = "GIT_HOOKS_BASE_PATH";
--- git-repo-manager/src/main.rs
+++ git-repo-manager/src/main.rs
@@ -1,26 +1,35 @@
+mod constants;
 mod service;
 
+use std::env::VarError;
+
+use constants::{GIT_HOOKS_BASE_PATH_KEY, GIT_REPOSITORY_BASE_PATH_KEY, SERVER_PORT_KEY};
 use dotenv::dotenv;
 use paastech_proto::gitrepomanager::git_repo_manager_server::GitRepoManagerServer;
 use service::{GitRepoManagerService, GitRepoManagerServiceConfig};
 use tonic::transport::Server;
 
-fn load_env_into_config() -> GitRepoManagerServiceConfig {
-    let git_repository_base_path_key = "GITSPROUT_GIT_REPOSITORY_BASE_PATH";
-    let githook_base_path_key = "GITSPROUT_GITHOOK_BASE_PATH";
-    GitRepoManagerServiceConfig {
-        git_repository_base_path: std::env::var(git_repository_base_path_key).unwrap(),
-        githook_base_path: std::env::var(githook_base_path_key).unwrap(),
-    }
+fn load_env_into_config() -> Result<GitRepoManagerServiceConfig, VarError> {
+    Ok(GitRepoManagerServiceConfig {
+        git_repository_base_path: std::env::var(GIT_REPOSITORY_BASE_PATH_KEY).unwrap_or_else(
+            |error| panic!("Err {}: env var {}", error, GIT_REPOSITORY_BASE_PATH_KEY),
+        ),
+        githook_base_path: std::env::var(GIT_HOOKS_BASE_PATH_KEY)
+            .unwrap_or_else(|error| panic!("Err {}: env var {}", error, GIT_HOOKS_BASE_PATH_KEY)),
+    })
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
-    let port_key = "GITSPROUT_SERVER_PORT";
-    let addr = format!("[::1]:{}", std::env::var(port_key).unwrap()).parse()?;
+    let addr = format!(
+        "[::1]:{}",
+        std::env::var(SERVER_PORT_KEY)
+            .unwrap_or_else(|error| panic!("Err {}: env var {}", error, SERVER_PORT_KEY))
+    )
+    .parse()?;
 
-    let gitsprout_config = load_env_into_config();
+    let gitsprout_config = load_env_into_config()?;
 
     let gitsprout_service = GitRepoManagerService {
         config: gitsprout_config,
